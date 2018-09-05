7a08f9d9a9727343b41f33dac402c3fe70bd6a32
Student <example@example.com>
2018-09-05 14:35:36 -0700

    Add routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import TicketList from "./TicketList";
+import NewTicketForm from "./NewTicketForm";
 import Header from "./Header";
+import { Switch, Route } from 'react-router-dom';
 
 function App(){
   return (
     <div>
       <Header/>
+      <Switch>
+        <Route exact path='/' component={TicketList} />
+        <Route path='/newticket' component={NewTicketForm} />
+      </Switch>
       <TicketList/>
     </div>
   );
