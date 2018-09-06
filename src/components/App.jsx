ac958e6fc3cd3d8035a717bc1a3b246a81598be7
Student <example@example.com>
2018-09-06 11:21:38 -0700

    Add error handling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,9 @@
-import React from "react";
-import TicketList from "./TicketList";
-import NewTicketForm from "./NewTicketForm";
-import Header from "./Header";
+import React from 'react';
+import TicketList from './TicketList';
+import NewTicketForm from './NewTicketForm';
+import Header from './Header';
 import { Switch, Route } from 'react-router-dom';
+import Error404 from './Error404';
 
 function App(){
   return (
@@ -11,8 +12,8 @@ function App(){
       <Switch>
         <Route exact path='/' component={TicketList} />
         <Route path='/newticket' component={NewTicketForm} />
+        <Route component={Error404} />
       </Switch>
-      <TicketList/>
     </div>
   );
 }
